20928b2bcdc75b8dd79514bf9f76cc02abb10325
fengyuanduan <917472315@qq.com>
2015-12-15 19:13:20 +0800

    spring的入门涉及jetty,maven

---
+++ webDemo/src/main/java/com/dfy/mvcdemo/controller/HelloMvcController.java
@@ -0,0 +1,18 @@
+package com.dfy.mvcdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/hello")
+public class HelloMvcController {
+	
+	@RequestMapping("/mvc")
+	//host:8080/hello/mvc
+	public String helloMvc(){
+		
+		
+		return "home";
+	}
+	
+}
